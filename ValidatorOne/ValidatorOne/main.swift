fb541b75b95cdfaa31d73c0e140d40342935bb92
Sam Krishna <sam.krishna@gmail.com>
2021-10-17 15:07:57 -0700

    Add "CharacterSetValidator"

--- ValidatorOne/ValidatorOne/main.swift
+++ ValidatorOne/ValidatorOne/main.swift
@@ -5,6 +5,8 @@
 //  Created by Sam Krishna on 10/17/21.
 //
 
+import Foundation
+
 protocol Validator {
     associatedtype ValidatedValue
     func validate(_: ValidatedValue) -> Bool
@@ -18,10 +20,26 @@ struct CharacterCountValidator : Validator {
     }
 }
 
+struct CharacterSetValidator : Validator {
+    let disallowedCharSet: CharacterSet
+
+    func validate(_ value: String) -> Bool {
+        let stringSet = CharacterSet(charactersIn: value)
+        let intersection = disallowedCharSet.intersection(stringSet)
+        return intersection.isEmpty
+    }
+}
 let ccv = CharacterCountValidator(range: 1...10)
-let trueOutcome = ccv.validate("yes")
-let falseOutcome = ccv.validate("hell no this shouldn\'t validate")
+var trueOutcome = ccv.validate("yes")
+var falseOutcome = ccv.validate("hell no this shouldn\'t validate")
 
 print("true outcome is \(trueOutcome)")
 print("false outcome is \(falseOutcome)")
 
+let csv = CharacterSetValidator(disallowedCharSet: CharacterSet(charactersIn: "abcde"))
+
+let trueCharSet = "fghij"
+let falseCharSet = "abc"
+
+print("character set validation for \(trueCharSet) is \(csv.validate(trueCharSet))")
+print("character set validation for \(falseCharSet) is \(csv.validate(falseCharSet))")
