bdd2e4535b6bddd4c36fc20df859342bbb233912
Sam Krishna <sam.krishna@gmail.com>
2022-02-16 22:17:33 -0800

    WIP: Create OrValidator #2

--- AssignmentTwo/AssignmentTwo/Validators/OrValidator.swift
+++ AssignmentTwo/AssignmentTwo/Validators/OrValidator.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct OrValidator<Value> : Validator {
+    let childValidator: [AnyValidator<Value>]
+
+    init<Child>(_ childValidator: Child) where Child : Validator, Child.Value == Value {
+        self.childValidator = [AnyValidator(childValidator)]
+    }
+
+    func validate(_ value: Value) -> Bool {
+        let validatedValues: [Bool] = childValidator.map { (validator) in
+            validator.validate(value)
+        }
+
+        return validatedValues.filter{$0 == true}.count > 0
+    }
+}
