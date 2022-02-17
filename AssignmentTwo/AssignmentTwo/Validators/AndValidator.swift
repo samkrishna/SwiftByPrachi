35c46a70370fc00fb1a23665fd22ceae2eda72d1
Sam Krishna <sam.krishna@gmail.com>
2022-02-17 10:45:09 -0800

    AND: Use for-loop to avoid expensive map and filter functions #2

--- AssignmentTwo/AssignmentTwo/Validators/AndValidator.swift
+++ AssignmentTwo/AssignmentTwo/Validators/AndValidator.swift
@@ -15,10 +15,12 @@ struct AndValidator<Value> : Validator {
     }
 
     func validate(_ value: Value) -> Bool {
-        let validatedValues: [Bool] = children.map { (validator) in
-            validator.validate(value)
+        for child in children {
+            if !child.validate(value) {
+                return false
+            }
         }
 
-        return validatedValues.filter{$0 == true}.count == children.count
+        return true
     }
 }
