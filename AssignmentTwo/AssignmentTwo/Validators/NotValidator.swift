8baffe4d75a5d253d902c25636ccf459189bd944
Sam Krishna <sam.krishna@gmail.com>
2022-02-16 22:17:10 -0800

    WIP: Create NotValidator #2

--- AssignmentTwo/AssignmentTwo/Validators/NotValidator.swift
+++ AssignmentTwo/AssignmentTwo/Validators/NotValidator.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct NotValidator<Value> : Validator {
+    let childValidator: AnyValidator<Value>
+
+    init<Child>(_ childValidator: Child) where Child : Validator, Child.Value == Value {
+        self.childValidator = AnyValidator(childValidator)
+    }
+
+    func validate(_ value: Value) -> Bool {
+        return !childValidator.validate(value)
+    }
+}
